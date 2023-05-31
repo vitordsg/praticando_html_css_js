f2e1c47fb3cc9aca0aa5971a65e03a894cb344e0
vitordsg <vitinho6.6@hotmail.com>
2023-05-31 05:28:38 -0300

    aplicando o script

--- ex2_idade/javascr.js
+++ ex2_idade/javascr.js
@@ -5,7 +5,7 @@ function enviar() {
     let anoNascimento = document.getElementById('txtnascimento')
     let resul = document.querySelector('div#resultado')
     
-
+    // PERGUNTA ANTES DE ENVIAR OS DADOS SE A CAIXA DE ANO DE NASCIMENTO EXISTE ALGUM VALOR OU O ANO E MAIOR QUE O ANO ATUAL
     if (anoNascimento.value.length == 0 || Number(anoNascimento.value) > ano) {
         alert('ERRO')
     }
@@ -17,25 +17,45 @@ function enviar() {
         let idade = ano - Number(anoNascimento.value) // ANO DIGITADO - MENOS O ATUAL / DEVOLVENDO A IDADE DA PESSOA
         let genero = '' // GENERO COMEÇA VAZIO
         let faixa = ''  // FAIXA COMEÇA VAZIA
-        var img = document.createElement('img')
-        img.setAttribute('id', 'photo')
+        var img = document.createElement('img') // CRIA UM ELEMENTO IMAGEM 'tagName'
+        img.setAttribute('id', 'photo') // ADICIONA ATRIBUTOS AO ELEMENTO 'nomeDoAtributo' 'valorDoAtributo'
 
 
         if (sexo[0].checked) { 
             genero = 'Masculino'
             
             if(idade >= 0 && idade < 5) {
-                faixa2 = 'Menino'
+
                 faixa = 'Bebê'
-                resul.innerHTML = `É um ${faixa2} ${faixa} do Gênero ${genero} de ${idade} anos<br>`
+                resul.innerHTML = `É um ${faixa} do Gênero ${genero} de ${idade} anos<br>`
                 img.setAttribute('src','../ex2_idade/imagem/bebem.png')
+                anoNascimento.value = ''
+                anoNascimento.focus()
             }
 
             else if (idade >= 5 && idade < 10) {
+
                 faixa = 'Criança'
-                resul.innerHTML = `É `
+                resul.innerHTML = `É uma ${faixa} do Gênero ${genero} de ${idade} anos<br>`
+                img.setAttribute('src', '../ex2_idade/imagem/criançam.png')
+                anoNascimento.value = ''
+                anoNascimento.focus()
             }
 
+            else if (idade <= 18) {
+                faixa = 'Adolescente'
+                resul.innerHTML = `É um ${faixa} do Gênero ${genero} de ${idade} anos<br>`
+                img.setAttribute('src', '../ex2_idade/imagem/jovemm.png')
+                anoNascimento.value = ''
+                anoNascimento.focus()
+            }
+
+            else if (idade < 50)
+                faixa = 'Jovem Adulto'
+                resul.innerHTML = `É um ${faixa} do Gênero ${genero} de ${idade} anos<br>`
+                img.setAttribute('src', '../ex2_idade/imagem/adultom.png')
+                anoNascimento.value = ''
+                anoNascimento.focus()
         }
 
         resul.appendChild(img)
