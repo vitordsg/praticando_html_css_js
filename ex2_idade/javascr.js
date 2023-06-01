1e6ed7938146b11fb97bc5a886e0b73b947270e4
vitordsg <vitinho6.6@hotmail.com>
2023-05-31 21:37:10 -0300

    estilos

--- ex2_idade/javascr.js
+++ ex2_idade/javascr.js
@@ -6,8 +6,8 @@ function enviar() {
     let resul = document.querySelector('div#resultado')
     
     // PERGUNTA ANTES DE ENVIAR OS DADOS SE A CAIXA DE ANO DE NASCIMENTO EXISTE ALGUM VALOR OU O ANO E MAIOR QUE O ANO ATUAL
-    if (anoNascimento.value.length == 0 || Number(anoNascimento.value) > ano || anoNascimento.value.length == 1) {
-        alert('ERRO')
+    if (anoNascimento.value.length != 4 || Number(anoNascimento.value) > ano || Number(anoNascimento.value < 1900)) {
+        alert('[ERRO] Insira os dados corretamente')
         anoNascimento.value = ''
         anoNascimento.focus()
     }
@@ -22,7 +22,7 @@ function enviar() {
         var img = document.createElement('img') // CRIA UM ELEMENTO IMAGEM 'tagName'
         img.setAttribute('id', 'photo') // ADICIONA ATRIBUTOS AO ELEMENTO 'nomeDoAtributo' 'valorDoAtributo'
 
-
+        
         if (sexo[0].checked) { 
             genero = 'Masculino'
             
@@ -59,7 +59,62 @@ function enviar() {
                 anoNascimento.value = ''
                 anoNascimento.focus()
             }
+
+            else {
+                faixa = 'Idoso'
+                resul.innerHTML = `É um ${faixa} do Gênero ${genero} de ${idade} anos<br>`
+                img.setAttribute('src' , '../ex2_idade/imagem/idosom.png')
+                anoNascimento.value = ''
+                anoNascimento.focus()
+            }
         }
+
+        if(sexo[1].checked) {
+            genero = 'Feminino'
+            
+            if(idade >= 0  && idade <= 5) {
+            faixa = 'Bebê'
+            resul.innerHTML = `É um ${faixa} do Gênero ${genero} de ${idade} anos<br>`
+            img.setAttribute('src', '../ex2_idade/imagem/bebef.png')
+            anoNascimento.value = ''
+            anoNascimento.focus()
+            }
+
+            else if (idade >= 5 && idade <= 10) {
+                faixa = 'Criança'
+                resul.innerHTML = `É uma ${faixa} do Gênero ${genero} de ${idade} anos<br>`
+                img.setAttribute('src' , '../ex2_idade/imagem/criançaf.png')
+                anoNascimento.value = ''
+                anoNascimento.focus()
+
+            }
+
+            else if (idade <= 18) {
+                faixa = 'Adolescente'
+                resul.innerHTML = `É uma ${faixa} do Gênero ${genero} de ${idade} anos<br>`
+                img.setAttribute('src' , '../ex2_idade/imagem/jovemf.png')
+                anoNascimento.value = ''
+                anoNascimento.focus()
+            }
+
+            else if (idade < 50) {
+                faixa = 'Jovem Adulto'
+                resul.innerHTML = `É uma ${faixa} do Gênero ${genero} de ${idade} anos<br>`
+                img.setAttribute('src' , '../ex2_idade/imagem/adultof.png')
+                anoNascimento.value = ''
+                anoNascimento.focus()
+            }
+
+            else {
+
+                faixa = 'Idosa'
+                resul.innerHTML = `É uma ${faixa} do Gênero ${genero} de ${idade} anos<br>`
+                img.setAttribute('src' , '../ex2_idade/imagem/idosof.png')
+                anoNascimento.value = ''
+                anoNascimento.focus()
+            }
+        }
+
 
         resul.appendChild(img)
         
